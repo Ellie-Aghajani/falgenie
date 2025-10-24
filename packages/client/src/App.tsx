df0a36e6284218f9b554712c0c6f6c20fd67c628
Ellie-Aghajani <z.aqajani@gmail.com>
2025-10-24 13:33:26 -0700

    feat: set prettier Resolves #7

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,22 @@
-import { useState, useEffect } from "react";
-import { Button } from "./components/ui/button";
+import { useState, useEffect } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState<string>("");
+   const [message, setMessage] = useState<string>('');
 
-  useEffect(() => {
-    fetch("api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-  return (
-    <>
-      
-      <div className="min-h-screen flex items-center justify-center">
-        <p className="text-lg font-bold p-4">{message}</p>
-        <Button >
-          Ask Falgenie
-        </Button>
-      </div>
-    </>
-  );
+   useEffect(() => {
+      fetch('api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
+   return (
+      <>
+         <div className="min-h-screen flex items-center justify-center">
+            <p className="text-lg font-bold p-4">{message}</p>
+            <Button>Ask Falgenie</Button>
+         </div>
+      </>
+   );
 }
 
 export default App;
