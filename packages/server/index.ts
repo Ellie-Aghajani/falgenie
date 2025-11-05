658b4837e34a75e4d5bb78671803e5a07e296d1e
Ellie-Aghajani <z.aqajani@gmail.com>
2025-11-04 16:53:50 -0800

    add '0.0.0.0' for render

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -18,6 +18,6 @@ app.use(router);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(Number(port), '0.0.0.0', () => {
    console.log(`Server is running at http://localhost:${port}`);
 });
