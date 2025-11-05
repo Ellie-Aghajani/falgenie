a625ef9ce9450c0e80d2b5eb869a3d54c87f1f3d
Ellie-Aghajani <z.aqajani@gmail.com>
2025-11-04 16:44:18 -0800

    add corse

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,18 @@
+import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
 dotenv.config();
 
 const app = express();
+// ✅ Allow requests only from frontend
+app.use(
+   cors({
+      origin: ['https://www.falgenie.com', 'http://localhost:5173'],
+      methods: ['GET', 'POST'],
+   })
+);
+
 app.use(express.json());
 app.use(router);
 
